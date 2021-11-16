f402c8eda63441247a059b3ea21e283a175f7061
雪域青竹 <yangjunst@sina.com>
2021-11-16 19:10:23 +0800

    source code of openjdk8-javac

---
+++ src/com/sun/HelloWorld.java
@@ -0,0 +1,19 @@
+package com.sun;
+
+/********************************************
+ * 开发人员: 雪域青竹
+ * 入职时间: 2016/05/16
+ * 开发时间: 2021/5/24 17:20
+ *********************************************/
+public class HelloWorld {
+    public static void main(String[] args) {
+        foo();
+    }
+
+    public static void foo(){
+        int a=0;
+        int b=6;
+        int c=130;
+        int d=33000;
+    }
+}
